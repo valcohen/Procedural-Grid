3328d3c558b6d900249a1e6c09071c930b97a705
Val Cohen <val@valcohen.com>
2018-05-28 21:21:40 -0700

    Fix UV mapping

--- Assets/MyGrid.cs
+++ Assets/MyGrid.cs
@@ -23,12 +23,16 @@ public class MyGrid : MonoBehaviour {
         mesh.name = "Procedural Grid";
 
         vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        Vector2[] uv = new Vector2[vertices.Length];
         for (int i = 0, y = 0; y <= ySize; y++ ) {
             for (int x = 0; x <= xSize; x++, i++) {
                 vertices[i] = new Vector3(x, y);
+                uv [i] = new Vector2 ((float)x / xSize, (float)y / ySize);
             }
         }
         mesh.vertices = vertices;
+        mesh.uv = uv;
+
 
         int[] triangles = new int[xSize * ySize * 6];
         for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++) {
