a0605d5e36ec59dcb974e2e8fd5b33d259cced93
Val Cohen <val@valcohen.com>
2018-05-28 21:28:14 -0700

    Add bump map, finish tutorial

--- Assets/MyGrid.cs
+++ Assets/MyGrid.cs
@@ -24,15 +24,18 @@ public class MyGrid : MonoBehaviour {
 
         vertices = new Vector3[(xSize + 1) * (ySize + 1)];
         Vector2[] uv = new Vector2[vertices.Length];
+        Vector4[] tangents = new Vector4[vertices.Length];
+        Vector4 tangent = new Vector4 (1f, 0f, 0f, -1f);
         for (int i = 0, y = 0; y <= ySize; y++ ) {
             for (int x = 0; x <= xSize; x++, i++) {
                 vertices[i] = new Vector3(x, y);
                 uv [i] = new Vector2 ((float)x / xSize, (float)y / ySize);
+                tangents [i] = tangent;
             }
         }
         mesh.vertices = vertices;
         mesh.uv = uv;
-
+        mesh.tangents = tangents;
 
         int[] triangles = new int[xSize * ySize * 6];
         for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++) {
